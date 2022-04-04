2be5ab2cce817b7d5836f2fe3bb7f1a117bd410d
lawlite <849451478@qq.com>
2022-04-04 23:22:16 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <olmap />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import olmap from './components/olmap.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    olmap
   }
 }
 </script>
 
 <style>
+*{padding:0; margin:0;}
+html,body{
+  height: 100%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/olmap.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="map" ref="rootmap">
+
+    </div>
+</template>
+
+<script>
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+export default {
+  data() {
+    return {
+      map: null
+    };
+  },
+  mounted() {
+    var mapcontainer = this.$refs.rootmap;
+    this.map = new Map({
+      target: "map",
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        projection: "EPSG:4326",    //使用这个坐标系
+        center: [114.064839,22.548857],  //深圳
+        zoom: 12
+      })
+    });
+  }
+};
+</script>
+
+<style>
+#map{height:100%;}
+/*隐藏ol的一些自带元素*/
+.ol-attribution,.ol-zoom { display: none;}
+
+</style>
