86aba965b546886df54faf6591d02c4357e41d98
lawlite <849451478@qq.com>
2022-04-05 21:31:47 +0800

    add geoserver wms layer

--- src/components/olmap.vue
+++ src/components/olmap.vue
@@ -9,6 +9,8 @@ import "ol/ol.css";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
+import TileWMS from "ol/source/TileWMS"
+import Tile from "ol/layer/Tile"
 export default {
   data() {
     return {
@@ -17,21 +19,39 @@ export default {
   },
   mounted() {
     var mapcontainer = this.$refs.rootmap;
+
+    // 自定义wms layer
+	var baseSource = new TileWMS({
+		url: 'http://127.0.0.1:1234/geoserver/higeo/wms',
+		params: {
+			'LAYERS': 'higeo:gdelt_part',
+			'TILED': true
+		},
+		serverType: 'geoserver'
+	});
+    var baseLayer = new TileLayer({
+		source: baseSource
+	});
+
+    // 添加layer
     this.map = new Map({
-      target: "map",
+      target: "map",  // 使用map div
       layers: [
-        new TileLayer({
-          source: new OSM()
-        })
+        new Tile({
+          source: new OSM()   // openstreet map地图
+        }),
+        baseLayer             // 自己定义的WMS服务
       ],
       view: new View({
         projection: "EPSG:4326",    //使用这个坐标系
         center: [114.064839,22.548857],  //深圳
-        zoom: 12
+        zoom: 7
       })
     });
   }
 };
+
+
 </script>
 
 <style>
